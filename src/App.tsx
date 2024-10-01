8be82264499517fd4641473a93ed78a35835ea2b
Habibi Med Wassim <habibimedwassim@gmail.com>
2024-10-02 00:56:09 +0100

    add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
 
   return (
     <>
       <h1> Welcome to Apartments Web Dashboard</h1>
+      <Button variant={"default"}> Click me</Button>
     </>
   )
 }
