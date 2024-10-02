98e3ff971edd4db54ade59771a49620f79221982
Habibi Med Wassim <habibimedwassim@gmail.com>
2024-10-02 03:58:09 +0100

    add dashboard components

---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,33 @@
+import { Monitor, Moon, Sun } from "lucide-react";
+
+import {
+  DropdownMenuItem,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useTheme } from "@/components/theme-provider";
+
+export function ModeToggle() {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenuSub>
+      <DropdownMenuSubTrigger>Theme</DropdownMenuSubTrigger>
+      <DropdownMenuSubContent>
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          <Sun className="h-4 w-4 mr-2" />
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          <Moon className="h-4 w-4 mr-2" />
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          <Monitor className="h-4 w-4 mr-2" />
+          System
+        </DropdownMenuItem>
+      </DropdownMenuSubContent>
+    </DropdownMenuSub>
+  );
+}
--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "@/pages/LoginPage";
-import HomePage from "@/pages/HomePage";
+import OwnerPage from "./pages/OwnerPage";
+import AdminPage from "./pages/AdminPage";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <HomePage/>
-    },
-    {
-        path: '/login',
-        element: <LoginPage/>
-    }
+  {
+    path: "/",
+    element: <LoginPage />, // Assuming this will be the main entry point initially
+  },
+  {
+    path: "/admin",
+    element: <AdminPage />,
+  },
+  {
+    path: "/owner",
+    element: <OwnerPage />,
+  },
 ]);
 
 export default router;
